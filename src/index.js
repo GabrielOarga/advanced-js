fd9b01c3061bf9d855b40f6f21a4cf2bdad5e8a1
goarga <goarga@stylyze.com>
2019-12-03 07:54:05 +0200

    Updated gitignore for vscode stuff

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-const name = 'tyler';
-
 console.log(name);
+console.log(handle);
+console.log(getUser);
+console.log(arrowFunc);
+
+const name = 'Tyler';
+const handle = '@tyler';
+
+function getUser() {
+    return {name: name, handle: handle}
+}
+
+const arrowFunc = () => {
+    return ({
+        name,
+        handle
+    })
+}
